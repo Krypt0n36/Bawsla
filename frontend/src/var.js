74b000fc985a453dbf5315f06515d087095d7775
krypt0n36 <belani.hassen@gmail.com>
2021-06-17 22:01:19 +0100

    req

--- frontend/src/var.js
+++ frontend/src/var.js
@@ -2,7 +2,7 @@
 
 
 export default {
-    backend_url:'http://localhost:5000',
-    frontend_url:'http://localhost:5000',
+    backend_url:'https://bawsla.herokuapp.com/',
+    frontend_url:'https://bawsla.herokuapp.com/',
     cdn_url:'https://bawsla.blob.core.windows.net'
 }
