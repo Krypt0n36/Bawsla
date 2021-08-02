4f6b77a099b20d2f0fc153989eaa4faf4df1eb82
krypt0n36 <belani.hassen@gmail.com>
2021-08-02 10:22:06 +0100

    fix

--- frontend/src/var.js
+++ frontend/src/var.js
@@ -5,7 +5,7 @@ export default {
     backend_url:'https://bawsla.herokuapp.com/',
     //frontend_url:'https://bawsla.herokuapp.com/',
     //backend_url:'http://localhost:5000/',
-    frontend_url:'http://localhost:3000/',  
+    frontend_url:'https://bawsla.herokuapp.com/',  
     cdn_url:'https://bawsla.blob.core.windows.net',
     //socketio_url:'http://localhost:4000'
 	socketio_url:'https://bawsla-chat-server.herokuapp.com/'
