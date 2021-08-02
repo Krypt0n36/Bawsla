ce6374881f8de9c52f6b8296f0484afdb58ce25c
krypt0n36 <belani.hassen@gmail.com>
2021-08-02 14:20:17 +0100

    falling back to http

--- frontend/src/var.js
+++ frontend/src/var.js
@@ -2,11 +2,11 @@
 
 
 export default {
-    backend_url:'http://bawsla.herokuapp.com/',
-    frontend_url:'http://bawsla.herokuapp.com/',
+    backend_url:'https://bawsla.herokuapp.com/',
+    frontend_url:'https://bawsla.herokuapp.com/',
     //backend_url:'http://localhost:5000/',
     //frontend_url:'http://localhost:3000/',  
     cdn_url:'https://bawsla.blob.core.windows.net',
     //socketio_url:'http://localhost:5000'
-	socketio_url:'ws://bawsla-chat-server.herokuapp.com/'
+	socketio_url:'wss://bawsla-chat-server.herokuapp.com/'
 }
